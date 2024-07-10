a3aa0dbc689e2ce79960b065c89322456c1dd40f
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-07-09 21:49:50 -0600

    Initial Commit

---
+++ FitnestX/FitnestXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnestXApp.swift
+//  FitnestX
+//
+//  Created by Modamori Oluwayomi on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct FitnestXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
