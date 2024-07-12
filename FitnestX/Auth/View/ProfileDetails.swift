b7222caaa73da877ba626359bc5d9b7d54255911
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-07-11 22:28:30 -0600

    Update ProfileDetails.swift

--- FitnestX/Auth/View/ProfileDetails.swift
+++ FitnestX/Auth/View/ProfileDetails.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ProfileDetails: View {
+    //Registration
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isChecked = false
+    
     @State private var selectedGender: String = ""
     @State private var dateOfBirth = Date.now
     @State private var weight: String = ""
     @State private var height: String = ""
     @State private var weightUnit: String = "KG"
     @State private var heightUnit: String = "CM"
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     
     let genders = ["Male", "Female"]
     var body: some View {
@@ -88,16 +97,43 @@ struct ProfileDetails: View {
                 }
                 Spacer()
                 CustomButtonView(isTapped: .constant(false), buttonBackgroundColor: Color.customPurple, buttonTextColor: Color.white, label: "Next") {
-                  
+                    if validateFields() {
+                        
+                    }else{
+                        showAlert = true
+                    }
                 }
             }
             .padding(.horizontal)
+            .onAppear{
+                loadRegistrationDetails()
+            }
+            .alert("Invalid Input", isPresented: $showAlert){
+                Button("OK", role: .cancel){}
+            }message: {
+                Text(alertMessage)
+            }
         }
         
                 
 
     }
     
+    private func validateFields() -> Bool {
+        if selectedGender.isEmpty || selectedGender ==  "Choose Gender"{
+            alertMessage =  "Please select a gender"
+            return false
+        }
+        if weight.isEmpty {
+            alertMessage = "Please enter yoye weight"
+            return false
+        }
+        if height.isEmpty {
+            alertMessage = "Please enter your height"
+            return false
+        }
+        return true
+    }
     private func toggleWeightUnit(){
         guard let currentWeight = Double(weight) else {return}
         
@@ -120,6 +156,20 @@ struct ProfileDetails: View {
                height = String(format: "%.2f", currentHeight * 30.48)
            }
     }
+    
+    
+    private func loadRegistrationDetails() {
+          firstName = UserDefaults.standard.string(forKey: "firstName") ?? ""
+          lastName = UserDefaults.standard.string(forKey: "lastName") ?? ""
+          email = UserDefaults.standard.string(forKey: "email") ?? ""
+        password =  UserDefaults.standard.string(forKey: "password") ?? ""
+        isChecked = UserDefaults.standard.bool(forKey: "isCheck")
+        print("Loaded Registration Details:")
+                print("First Name: \(firstName)")
+                print("Last Name: \(lastName)")
+                print("Email: \(email)")
+        print("password \(password)")
+      }
 }
 
 #Preview {
