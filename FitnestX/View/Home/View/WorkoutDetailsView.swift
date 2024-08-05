6b61084bccf4986872af88980d51b76b254ea453
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-08-05 16:50:21 -0600

    Update WorkoutDetailsView.swift

--- FitnestX/View/Home/View/WorkoutDetailsView.swift
+++ FitnestX/View/Home/View/WorkoutDetailsView.swift
@@ -8,23 +8,48 @@
 import SwiftUI
 
 struct WorkoutDetailsView: View {
-    //    var workout: WorkoutData?
+    @Environment (\.dismiss) var dismiss
     
     var body: some View {
         ScrollView {
             VStack {
                 ReuseableChart(pastWeekLineBackground: Color.customWhite.opacity(0.7), currentWeekLineColor: Color.customWhite, lineBackground: Color.customBlue, lineGradientColorStart: Color.customWhite.opacity(0.8), lineGradientColorStop: Color.customWhite.opacity(0.2), pastWeekLineWidth: 2, currentWeekLineWidth: 4, chartBackGround: Color.clear)
                     .frame(height: 300)
-                Spacer()
                 
             }
+            .padding()
+            .navigationTitle("Notification")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image("Back-Navs")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(Color.customGrey)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image("More Circle")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(Color.customGrey)
+                    }
+                    
+                }
+        }
             
         }
         .background{
-            Color.customBlue
-            //            .padding(.horizontal)
+            Color.customBlue.ignoresSafeArea()
         }
-        .ignoresSafeArea(edges: .top)
+        
     }
 }
 #Preview {
